6df8b9feaadc4b19ff3bc3fc02597e2cfc9421ab
Jan Meischner <jan.meischner@googlemail.com>
2018-01-29 16:38:07 +0100

    Added Test for ToDoCoFiles.toYaml()

--- Tests/ToDoCoConfigTests/WriteTests.swift
+++ Tests/ToDoCoConfigTests/WriteTests.swift
@@ -19,6 +19,22 @@ class WriteConfigFile: QuickSpec {
                 expect(yaml["project"]["name"].string).to(equal("Project Name"))
                 expect(yaml["project"]["author"].string).to(equal("Project Author"))
             }
+        };
+
+        describe("ToDoCoFiles toYaml") {
+            it("should result in a valid yaml string") {
+                let files = ToDoCoFiles()
+                let yamlString = files.toYaml()
+                var yaml: Yaml = Yaml.string("")
+
+                do {
+                    yaml = try Yaml.load(yamlString)
+                } catch {}
+
+                expect(yaml["files"]["useGitignore"].bool).to(beFalsy())
+                expect(yaml["files"]["ignore"].array).to(beNil())
+                expect(yaml["files"]["add"].array).to(equal(["**"]))
+            }
         }
     }
 }
