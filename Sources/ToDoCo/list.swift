2ba572e3c3d93437517d2963be67e0046713389d
Jan <jan.meischner@adesso.de>
2018-02-26 08:52:02 +0100

    Better print of files list

--- Sources/ToDoCo/list.swift
+++ Sources/ToDoCo/list.swift
@@ -4,19 +4,37 @@ import ToDoCoConfig
 import ToDoCoReader
 import Rainbow
 
+let dashes30 = String(repeating: "-", count: 30).yellow
+
 func listFileWhichAreNotIgnored(atPath: String) {
     do {
         let ignore = ToDoCoIgnore(atRoot: atPath)
         let files = try ignore.files()
+        let projectDirectory = URL(fileURLWithPath: atPath).lastPathComponent
 
-        for file in files {
-            print(file)
+        let directories = Dictionary(grouping: files, by: {
+            URL(fileURLWithPath: $0)
+                .pathComponents
+                .drop { $0 != projectDirectory }
+                .dropLast()
+                .reduce("", { $0 + "/" +  $1 })
+        }).sorted { $0.key < $1.key }
+
+        print("\(dashes30) " + "Files".green + " \(dashes30)")
+        for (dir, files) in directories {
+            print("\u{25B8} \(dir):".cyan)
+            for file in files {
+                let filename = URL(fileURLWithPath: file).lastPathComponent
+                print("  \(filename)")
+            }
+            print()
         }
+        print(String(repeating: "-", count: 67).yellow)
     } catch ToDoCoConfigError.ignoreFileCouldNotBeRead {
         print("Error: It was not possible to read \(ToDoCoNames.ignoreFile).".red)
     } catch {
         print("An Error Occured: \(error)".red)
-    } 
+    }
 }
 
 func listToDos(atPath: String) {
@@ -25,8 +43,6 @@ func listToDos(atPath: String) {
         let reader = ToDoCoReader(ignore: ignore)
         let todos = try reader.read()
         let todosByFiles = Dictionary(grouping: todos, by: { $0.file })
-
-        let dashes30 = String(repeating: "-", count: 30).yellow
 
         print("\(dashes30) " + "Todos".green + " \(dashes30)")
 
